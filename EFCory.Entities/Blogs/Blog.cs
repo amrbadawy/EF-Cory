5500c50f9df053d7672716eafa2997d742a0fafe
Amr Mahmoud Badawy <abadawi@elm.sa>
2021-05-26 20:40:56 +0300

    Add PropertyMetadata

--- EFCory.Entities/Blogs/Blog.cs
+++ EFCory.Entities/Blogs/Blog.cs
@@ -8,5 +8,31 @@ namespace EFCory.Entities.Blogs
         public string Name { get; set; }
         public List<Post> Posts { get; set; } = new List<Post>();
         public DateTime CreatedAt { get; set; }
+
+        public static class Metadata
+        {
+            public static readonly StringPropertyMetadata Name = new()
+            {
+                IsRequired = true,
+                MaxLength = 250
+            };
+        }
+    }
+
+
+
+    public class PropertyMetadata 
+    {
+        public bool IsRequired { get; init; } = false;
+    }
+
+    public class StringPropertyMetadata: PropertyMetadata
+    {
+        public int MaxLength { get; init; }
+    }
+    public class DecimalPropertyMetadata: PropertyMetadata
+    {
+        public int Precision { get; init; }
+        public int Scale { get; init; }
     }
 }
--- EFCory.EntityFramework/Configurations/Blogs/BlogConfiguration.cs
+++ EFCory.EntityFramework/Configurations/Blogs/BlogConfiguration.cs
@@ -12,9 +12,16 @@ namespace UPD.EntityFramework.Configurations
             builder.HasKey(e => e.Id);
 
             builder.HasIndex(e => e.Name).IsUnique();
-            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(Blog.Metadata.Name.MaxLength).IsRequired();
 
             builder.Property(rp => rp.CreatedAt).HasDefaultCurrentDate();
         }
     }
+    //public static class PropertyBuilderExtensions<TProperty>
+    //{
+    //    public virtual PropertyBuilder<TProperty> HasPropertyMetadata(int maxLength)
+    //    {
+
+    //    }
+    //}
 }
